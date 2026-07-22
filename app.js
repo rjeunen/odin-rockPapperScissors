208c4f246bd638b3f1c89a840bf6517eb32b89a8
Robby Jeunen <robbyjeunen@gmail.com>
2026-07-22 18:10:45 +0200

    implemented functionality to play a single round

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 //Get the computer choice - random choice
 function getComputerChoice(){
     //Generate a random number from 1 - 3
@@ -21,7 +24,26 @@ function getComputerChoice(){
 function getHumanChoice(){
     //Ask the user for an input
     let humanChoice = prompt("Enter your choice: rock, paper or scissors");
+    humanChoice = humanChoice.toLowerCase();
     return humanChoice;
+}
+
+//play a single round
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        console.log(`It's a draw, you both picked ${humanChoice}`);
+    }
+    else if(humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice === "rock"
+    ){
+        console.log(`You win, ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    }
+    else{
+        console.log(`You lose, ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
 }
 
 //Testing
